155fb07169d184ffff28153cec55914a84a97d77
GeniusNinja33 <akagishotaro44@gmail.com>
2022-06-06 00:54:05 +0900

    fix:dynamic import

--- src/view/index.ts
+++ src/view/index.ts
@@ -1,14 +1,8 @@
 import dynamic from "next/dynamic";
 
 //-------------------------------------------------------------
-// export const Hero = dynamic(() => import("./Hero/Hero"));
-// export const Network = dynamic(() => import("./Network/Network"));
-// export const NFTMeet = dynamic(() => import("./NFTMeet/NFTMeet"));
-// export const Roadmap = dynamic(() => import("./Roadmap/Roadmap"));
-// export const FAQ = dynamic(() => import("./FAQ/FAQ"));
-
-export { default as Hero } from "./Hero/Hero";
-export { default as Network } from "./Network/Network";
-export { default as NFTMeet } from "./NFTMeet/NFTMeet";
-export { default as Roadmap } from "./Roadmap/Roadmap";
-export { default as FAQ } from "./FAQ/FAQ";
+export const Hero = dynamic(() => import("./Hero/Hero"));
+export const Network = dynamic(() => import("./Network/Network"));
+export const NFTMeet = dynamic(() => import("./NFTMeet/NFTMeet"));
+export const Roadmap = dynamic(() => import("./Roadmap/Roadmap"));
+export const FAQ = dynamic(() => import("./FAQ/FAQ"));
