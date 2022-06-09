65550abe0f9cb7d7bf0dd879675498c0002b4976
GeniusNinja33 <akagishotaro44@gmail.com>
2022-06-09 19:14:21 +0900

    fix:loading erro

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,6 +18,14 @@ function MyApp({ Component, pageProps }: AppProps) {
     });
   }, []);
 
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles?.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <AppLayout>
       <Component {...pageProps} />
